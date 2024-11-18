0f19cd68de61a0d3b8a29e0235f0b043e8e0d1a5
jesus <jesperezpe@unal.edu.co>
2024-11-18 04:43:09 -0500

    aplicacion python taller 3

---
+++ televisores/control.py
@@ -0,0 +1,44 @@
+from tv import TV
+
+class Control:
+    def __init__(self,tv):
+        self.tv = tv 
+        pass
+
+    def enlazar(self,tv):
+        self.tv = tv
+        tv.setControl(self)
+
+    def getTv(self):
+        return self.tv
+    
+    def setTv(self,tv):
+        self.tv = tv
+
+    def canalUp():
+        if TV.getCanal() < 120:
+            TV.canalUp()
+
+    def canalDown():
+        if TV.getCanal() > 1:
+            TV.canalDown()
+
+    def volumenUp():
+        if TV.getVolumen() < 7:
+            TV.volumenUp()
+
+    def volumenDown():
+        if TV.getVolumen()> 0:
+            TV.volumenDown()
+
+    def turnOn():
+        TV.turnOn()
+
+    def turnOff():
+        TV.turnOff()
+
+    def setCanal(self,canal):
+        TV.setCanal(canal)
+
+    def setVolumen(self,volumen):
+        TV.setVolumen(volumen)
---
+++ televisores/tv.py
@@ -0,0 +1,73 @@
+class TV:
+    numTV = 0
+
+    def __init__(self,marca,estado):
+        self._marca = marca
+        self.estado = estado
+        self.canal = 1
+        self.volumen = 1
+        self._precio = 500
+        self.control = ""
+
+        pass
+
+    def getNumTV():
+        return TV.numTV
+    
+    def setNumTV(numTV):
+        TV.numTV = numTV
+
+    def canalUp(self):
+        if self.canal < 120:
+            self.canal +=1
+
+    def canalDown(self):
+        if self.canal > 1:
+            self.canal -= 1
+
+    def volumenUp(self):
+        if self.volumen < 7:
+            self.volumen += 1
+
+    def volumenDown(self):
+        if self.volumen > 0:
+            self.volumen -= 1
+
+    def turnOn(self):
+        self.estado = True
+
+    def turnOff(self):
+        self.estado = False
+
+    def getEstado(self):
+        return self.estado
+
+    def getMarca(self):
+        return self._marca
+    
+    def getCanal(self):
+        return self.canal
+    
+    def getPrecio(self):
+        return self._precio
+    
+    def getVolumen(self):
+        return self.volumen
+    
+    def getControl(self):
+        return self.control
+
+    def setMarca(self,marca):
+        self._marca = marca
+
+    def setCanal(self,canal):
+        self.canal = canal
+    
+    def setPrecio(self,precio):
+        self._precio = precio
+    
+    def setVolumen(self,volumen):
+        self.volumen = volumen
+
+    def setControl(self,control):
+        self.control = control
